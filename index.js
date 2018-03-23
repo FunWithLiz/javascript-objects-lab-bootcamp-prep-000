50631f27b24abb0bddc35c839fa24372a17ffeba
Elizabeth Fun <elizabethfun@gmail.com>
2018-03-23 16:10:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var recipes = {a:"z", b:"y", c:"x"};
+
+function updateObjectWithKeyAndValue(obj, key, value){
+return Object.assign({}, obj, {[key]:value})
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+  obj[key] = value
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key){
+  delete obj.key
+  return obj
+}
+
+function destructivelyDeleteFromObjectByKey(obj, key){
+  return obj
+}
